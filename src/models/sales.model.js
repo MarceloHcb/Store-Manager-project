0d4ee2a19225764e5607ca095716770dce5b6aa1
MarceloHcb <marcelohcb@msn.com>
2023-04-27 15:02:37 -0300

    fix promise.all

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,19 +1,34 @@
 const connection = require('./connection');
 
-const registerDate = async () => {
-  await connection.execute('INSERT INTO StoreManager.sales (date) VALUES (NOW())');
+const registerDate = async () => { 
+    await connection.execute('INSERT INTO StoreManager.sales (date) VALUES (NOW())');
   const [saleId] = await connection
     .execute('SELECT id FROM StoreManager.sales ORDER BY id DESC LIMIT 1');
   return saleId;
 };
+// const registerSale = async (sales) => {
+//   await connection.execute('INSERT INTO StoreManager.sales (date) VALUES (NOW())');
+//   const [{ id }] = await registerDate();  
+//   console.log(id);
+//   sales.map((sale) => {
+//   const { productId, quantity } = sale;  
+//   connection.execute(`INSERT INTO StoreManager.sales_products 
+// (sale_id, product_id, quantity) VALUES(?,?,?)`, [id, productId, quantity]);
+//   });
+//   return { id, itemsSold: [...sales] };
+// };
 
 const registerSale = async (sales) => {
   const [{ id }] = await registerDate();  
-  sales.map(async (sale) => {
-  const { productId, quantity } = sale;  
-  await connection.execute(`INSERT INTO StoreManager.sales_products 
-(sale_id, product_id, quantity) VALUES(?,?,?)`, [id, productId, quantity]);
+  const promises = sales.map((sale) => {
+    const { productId, quantity } = sale;
+    return connection
+      .execute(`INSERT INTO StoreManager.sales_products 
+      (sale_id, product_id, quantity) VALUES(?,?,?)`, [id, productId, quantity]);
   });
+  
+  await Promise.all(promises);
+  
   return { id, itemsSold: [...sales] };
 };
 module.exports = {
