871197ff3859c2b5800ba10eb7cd600028ca6844
MarceloHcb <marcelohcb@msn.com>
2023-04-29 00:09:50 -0300

    refactory sales.service

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -11,7 +11,7 @@ const getSaleById = async (id) => {
   return { type: null, message: sale };
 };
 
-const registerSale = async (sales) => {
+const validateSales = async (sales) => {
   const allProducts = await productModel.getAllProducts();
   const errors = sales.map((sale) => validationSales(sale));  
   const hasErrors = errors.some((err) => err.type !== null);
@@ -25,26 +25,23 @@ const registerSale = async (sales) => {
   if (hasProductNotFound) {
     return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
   }
+  return null;
+};
 
+const registerSale = async (sales) => {
+  const error = await validateSales(sales);
+  if (error) {
+    return error;
+  }
   const sale = await salesModel.registerSale(sales);
   return { type: null, message: sale };
 };
 
 const updateSale = async (id, sales) => {
-const allProducts = await productModel.getAllProducts();
-  const errors = sales.map((sale) => validationSales(sale));  
-  const hasErrors = errors.some((err) => err.type !== null);
-  if (hasErrors) {
-    const error = errors.find((err) => err.type !== null);    
+  const error = await validateSales(sales);
+  if (error) {
     return error;
   }
-  const productIds = allProducts.map((prod) => prod.id);
-  const hasProductNotFound = sales
-    .some(({ productId }) => productId && !productIds.includes(productId));
-  if (hasProductNotFound) {
-    return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
-  }
-
   const newSale = await salesModel.updateSale(id, sales);  
   return { type: null, message: newSale };
 };
